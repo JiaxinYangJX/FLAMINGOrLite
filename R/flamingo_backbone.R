510bfe78b64152f188aa4fbf4e4b152a504b6d09
XinruiYu <118102310+xiaoYU2001@users.noreply.github.com>
2024-11-19 11:12:33 -0500

    Update flamingo_backbone.R

--- R/flamingo_backbone.R
+++ R/flamingo_backbone.R
@@ -1,4 +1,17 @@
-
+#' backbone reconstruction of flamingorlite
+#' Core function for 3D genome structure reconstruction using low-rank matrix completion
+#' @param temp_folder tempfolder.
+#' @param sample_rate Fraction of available entries in Hi-C to be used during the reconstruction. Default = 0.75.
+#' @param lambda Weights for all sampled entries. Default = 10.
+#' @param r Weights for distance between consecutive points. Default = 1.
+#' @param max_dist Maximum allowed distance betwee two consecutive points. Default = 0.1
+#' @param error_threshold Error thresholds for reconstruction. Default = 1e-4.
+#' @param max_iter Maximum iterations. Default = 500.
+#' @param alpha Convertion factor between interaction frequency and pairwise distance. Default = -0.25.
+#' @param inf_dist Maximun allowed distance betwee any two points. Default = 3.
+#' @keywords flamingo_backbone
+#' @return A flamingo_prediction object containing the fragment id and 3D coordinates
+#' @export
 flamingo_backbone <- function(temp_folder,
                               sample_rate,
                               lambda,
