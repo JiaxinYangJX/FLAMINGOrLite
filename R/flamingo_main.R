f212dc803858ab885d6a7b7c6e54c04f6871d899
XinruiYu <118102310+xiaoYU2001@users.noreply.github.com>
2024-11-19 17:12:32 -0500

    Update flamingo_main.R

--- R/flamingo_main.R
+++ R/flamingo_main.R
@@ -46,7 +46,7 @@ flamingo_main <- function(hic_data,
 
   #### create temp folder
   
-  temp_folder = paste0("./temp")
+  temp_folder = paste0("./temp",b)
   print(temp_folder)
   dir.create(temp_folder)
   dir.create(paste0(temp_folder,'/domain_data'))
